b78b5de444fe6984ba4b75b7f32313c62ee92d4d
z974886437 <164758459+z974886437@users.noreply.github.com>
2025-11-23 16:07:24 +0800

    Attack animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,13 +11,14 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 3.5f;
     [SerializeField] private float jumpForce = 8;
     private float xInput;
+    private bool facingRight = true;
+    private bool canMove = true;
+    private bool canJump = true;
 
     [Header("Collision details")] 
     [SerializeField] private float groundCheckDistance;
-    
-    [SerializeField] private bool facingRight = true;
-
-    private int a;
+    [SerializeField] private LayerMask whatIsGrounded;
+    private bool isGrounded;
     
     
 
@@ -29,36 +30,64 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleCollision();
         HandleInput();
         HandleMovement();
         HandleAnimations();
         HandleFlip();
     }
 
+    public void EnableMovementAndJump(bool enable)
+    {
+        canMove = enable;
+        canJump = enable;
+    }
+
     private void HandleAnimations()
     {
-        bool isMoving = rb.linearVelocity.x != 0;
-        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("xVelocity",rb.linearVelocity.x);
+        anim.SetFloat("yVelocity",rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
 
     }
 
     private void HandleInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+        
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump();
-        }
+            TryToJump();
+        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+            TryToAttack();
     }
 
+    private void TryToAttack()
+    {
+        if (isGrounded)
+            anim.SetTrigger("attack");
+           
+        
+    }
+    
+    private void TryToJump()
+    {
+        if(isGrounded && canJump)
+          rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+    
     private void HandleMovement()
     {
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        if(canMove)
+            rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        else
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
     }
 
-    private void Jump()
+    private void HandleCollision()// 使用射线检测物体是否接触地面
     {
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        // 从物体当前位置向下发射射线，检测是否接触到地面
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGrounded);
     }
 
     private void HandleFlip()
@@ -73,5 +102,10 @@ public class Player : MonoBehaviour
     {
         transform.Rotate(0,180,0);// 通过旋转模型180度，达到水平翻转的效果
         facingRight = !facingRight; // 切换角色朝向状态
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position,transform.position + new Vector3(0,-groundCheckDistance));// 绘制一条从物体位置到物体位置下方某一距离的线
     }
 }
---
+++ Assets/PlayerAnimationEvents.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerAnimationEvents : MonoBehaviour
+{
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+    private void DisableMovementAndJump() => player.EnableMovementAndJump(false);
+    
+    private void EnableMovementAndJump() => player.EnableMovementAndJump(true);
+    
+    
+  
+}
